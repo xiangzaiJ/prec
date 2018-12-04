866226802564f1a0c0c9551f25281d37de07e20f
zyx <13337513591@163.com>
2018-12-04 22:29:47 +0800

    2018/12/4搭建ssm用户模块环境

--- prec-pojo/src/main/java/com.prec.pojo/PrUser.java
+++ prec-pojo/src/main/java/com.prec.pojo/PrUser.java
@@ -1,11 +1,162 @@
 package com.prec.pojo;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author 张业祥
  */
+@Table(name = "pr_user")
 public class PrUser implements Serializable  {
 
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(generator = "JDBC")
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String email;
+
+    @Column
+    private String name;
+
+    @Column
+    private String headPic;
+
+    @Column
+    private boolean sex;
+
+    @Column
+    private Date LastLoginTime;
+
+    @Column
+    private Date birthday;
+
+    @Column
+    private Date createTime;
+
+    @Column
+    private Date modifyTime;
+
+    @Column
+    private boolean isDelete;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeadPic() {
+        return headPic;
+    }
+
+    public void setHeadPic(String headPic) {
+        this.headPic = headPic;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public Date getLastLoginTime() {
+        return LastLoginTime;
+    }
+
+    public void setLastLoginTime(Date lastLoginTime) {
+        LastLoginTime = lastLoginTime;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public boolean isDelete() {
+        return isDelete;
+    }
+
+    public void setDelete(boolean delete) {
+        isDelete = delete;
+    }
 }
